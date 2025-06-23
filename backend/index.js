e72eee67b75b84bf4c522b675e1576595676ddea
Fahad <145900785+Fahad00001@users.noreply.github.com>
2025-06-23 20:01:25 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["https://onecart-frontend-fqh6.onrender.com", "http://localhost:5174"],
+    origin: ["https://onecart-frontend-fqh6.onrender.com", "https://onecart-admin-9sc9.onrender.com"],
     credentials: true,
   })
 );
